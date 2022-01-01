1959f48ae697e0db4f5f7d28637b7ef3439be68d
sai3639 <91225513+sai3639@users.noreply.github.com>
2021-12-31 18:25:23 -0600

    Add files via upload

---
+++ nameGame.py
@@ -0,0 +1,91 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 19 14:19:38 2021
+
+@author: saira
+
+"""
+
+# Date:              22 October 2021
+#name game
+
+name = input("What is your name? ")
+y = name[1:]
+
+if name[0] == 'A':
+    first = name.lower()
+    #first.lower()
+    print(name + ', ' + name + ', ' + 'Bo-B' + first)
+    print("Banana-Fana Fo-F" + first)
+    print("Me Mi Mo-M" + first)
+    print(name + "!")
+elif name[0] == 'E':
+    first = name.lower()
+    #first.lower()
+    print(name + ', ' + name + ', ' + 'Bo-B' + first)
+    print("Banana-Fana Fo-F" + first)
+    print("Me Mi Mo-M" + first)
+    print(name + "!")
+elif name[0] == "I":
+    first = name.lower()
+    #first.lower()
+    print(name + ', ' + name + ', ' + 'Bo-B' + first)
+    print("Banana-Fana Fo-F" + first)
+    print("Me Mi Mo-M" + first)
+    print(name + "!")
+elif name[0] == 'O':
+    first = name.lower()
+    #first.lower()
+    print(name + ', ' + name + ', ' + 'Bo-B' + first)
+    print("Banana-Fana Fo-F" + first)
+    print("Me Mi Mo-M" + first)
+    print(name + "!")
+elif name[0] == 'U':
+    first = name.lower()
+    #first.lower()
+    print(name + ', ' + name + ', ' + 'Bo-B' + first)
+    print("Banana-Fana Fo-F" + first)
+    print("Me Mi Mo-M" + first)
+    print(name + "!")
+elif name[1] == "r":
+    #first = name.remove('B')
+    y = name[2:]
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
+elif name[1] == "j":
+    #first = name.remove('B')
+    y = name[2:]
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
+elif name[1] == "h":
+    #first = name.remove('B')
+    y = name[2:]
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
+elif name[1] == "c":
+    #first = name.remove('B')
+    y = name[2:]
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
+elif name[1] == "h":
+    #first = name.remove('B')
+    y = name[2:]
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
+    
+    
+else:
+    print(name + ', ' + name + ', ' + 'Bo-B' + y)
+    print("Banana-Fana Fo-F" + y)
+    print("Me Mi Mo-M" + y)
+    print(name + "!")
